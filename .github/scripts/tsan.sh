b5258176fa4b653a3e8a2c7a19d07f201087e448
Travis Sharp <2798069+tsharp@users.noreply.github.com>
2026-06-27 16:36:43 -0700

    fix additional tests

--- .github/scripts/tsan.sh
+++ .github/scripts/tsan.sh
@@ -2,12 +2,23 @@
 
 set -ex
 
+toolchain="${nightly:-nightly-2026-04-16}"
+
+rustup toolchain install "${toolchain}" --component rust-src
+
 export ASAN_OPTIONS="detect_odr_violation=0 detect_leaks=0"
+packages=(
+    -p nacelle-core
+    -p nacelle-tcp
+    -p nacelle-http
+    -p nacelle
+    -p nacelle-stress-common
+)
 
 # Run address sanitizer
 RUSTFLAGS="-Z sanitizer=address" \
-cargo test --target x86_64-unknown-linux-gnu --test test_bytes --test test_buf --test test_buf_mut
+cargo +"${toolchain}" test "${packages[@]}" --lib --target x86_64-unknown-linux-gnu
 
 # Run thread sanitizer
 RUSTFLAGS="-Z sanitizer=thread" \
-cargo -Zbuild-std test --target x86_64-unknown-linux-gnu --test test_bytes --test test_buf --test test_buf_mut
+cargo +"${toolchain}" -Zbuild-std test "${packages[@]}" --lib --target x86_64-unknown-linux-gnu
